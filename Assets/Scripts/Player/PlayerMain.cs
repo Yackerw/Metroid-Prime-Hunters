a59f0778e35c73b30d68bbf08ec71d77ace27f4e
Yackerw <flippystar@gmail.com>
2022-05-16 05:06:30 -0400

    Fix minor issue

--- Assets/Scripts/Player/PlayerMain.cs
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMain : MonoBehaviour
 {
-    enum PlayerType : int { local, network, bot }
+    public enum PlayerType : int { local, network, bot }
 
 
     Rigidbody player;
